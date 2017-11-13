a990163a9d7b424a95b33ad6750705c5cde79520
Jules Randolph <jules.sam.randolph@gmail.com>
2017-11-13 15:08:54 -0500

    initial commit

---
+++ module-resolution.test.ts
@@ -0,0 +1,20 @@
+/*
+* Notice that typescript compiler does not complain : it expects implementation to comply with typedefs
+*/
+import unexportedReadbleColor from 'polished/lib/color/readableColor'
+
+/*
+ * The following work:
+ *
+ * import readbleColor = require('polished/lib/color/readableColor')
+ *
+ * or
+ *
+ * import * as readbleColor from 'polished/lib/color/readableColor'
+ */
+
+describe('readableColor module', () => {
+    it('should be exported to default', () => {
+        expect(unexportedReadbleColor).toBeDefined()
+    })
+})
---
+++ readme.md
@@ -0,0 +1,11 @@
+#  Mismatch between type definitions and effective module export policy (polished)
+
+Just run
+
+```
+yarn install
+```
+and
+```
+yarn test
+```
